ea378bf0a62f4c004839260439cdd4da74100128
trumfa <jordibabi@gmail.com>
2026-08-08 19:17:13 +0200

    Add files via upload

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,18 +3,24 @@ import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 import alpinejs from "@astrojs/alpinejs";
-import vercel from "@astrojs/vercel/serverless";
+import node from "@astrojs/node";
+import vercel from "@astrojs/vercel";
 import { i18n, filterSitemapByDefaultLocale } from "astro-i18n-aut/integration";
 import { DEFAULT_LOCALE, LOCALES, SITE_URL } from "./src/consts";
 
 const defaultLocale = DEFAULT_LOCALE;
 const locales = LOCALES;
 
+const isVercel = process.env.VERCEL === "1" || process.env.VERCEL === "true" || !!process.env.VERCEL_ENV;
+
 // https://astro.build/config
 export default defineConfig({
+  redirects: {
+    '/open-brand': '/gymcreamer',
+  },
 	site: SITE_URL,
 	output: "hybrid",
-	adapter: vercel(),
+	adapter: isVercel ? vercel() : node({ mode: "standalone" }),
 	trailingSlash: "ignore",
 	build: {
 		format: "directory",
