496d0e26a3805af1afe8f5d0e6da846712dac7ca
trumfa <jordibabi@gmail.com>
2026-08-11 00:12:32 +0200

    Add files via upload

--- src/consts.ts
+++ src/consts.ts
@@ -2,7 +2,7 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 // Website metadata
-export const SITE_URL: string = "https://gymcream.com";
+export const SITE_URL: string = "https://gymcream.me";
 export const SITE_TITLE: string = "Gym Cream";
 export const SITE_DESCRIPTION: string = "Trenca les regles - Marca independent d'equipament i apparel de gimnàs.";
 
