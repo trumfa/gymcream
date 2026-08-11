0defb467f18638728def793d7bb09cd7c66b5e46
trumfa <jordibabi@gmail.com>
2026-08-11 19:20:13 +0200

    Add files via upload

---
+++ src/astro.config.mjs
@@ -0,0 +1,48 @@
+import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
+import tailwindcss from "@tailwindcss/vite";
+import alpinejs from "@astrojs/alpinejs";
+import node from "@astrojs/node";
+import vercel from "@astrojs/vercel/serverless";
+import { i18n, filterSitemapByDefaultLocale } from "astro-i18n-aut/integration";
+import { DEFAULT_LOCALE, LOCALES, SITE_URL } from "./src/consts";
+
+const defaultLocale = DEFAULT_LOCALE;
+const locales = LOCALES;
+
+const isVercel = process.env.VERCEL === "1" || process.env.VERCEL === "true" || !!process.env.VERCEL_ENV;
+
+// https://astro.build/config
+export default defineConfig({
+  redirects: {
+    '/open-brand': '/gymcreamer',
+  },
+	site: SITE_URL,
+	output: "static",
+	adapter: isVercel ? vercel() : node({ mode: "standalone" }),
+	trailingSlash: "ignore",
+	build: {
+		format: "directory",
+	},
+	server: {
+		host: "0.0.0.0",
+		port: 3000,
+	},
+	vite: {
+		logLevel: "error",
+		define: {
+			__DATE__: `'${new Date()}'`,
+		},
+		plugins: [tailwindcss()],
+	},
+	integrations: [
+		mdx(),
+		alpinejs(),
+		i18n({
+			locales,
+			defaultLocale,
+			exclude: ["pages/api/**/*", "src/pages/api/**/*", "api/**/*", "pages/rss.xml.ts", "pages/[locale]/rss.xml.ts"],
+		}),
+	],
+});
