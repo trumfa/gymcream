712ded17349e19a9db0a7f69134e0bf5c276b760
trumfa <jordibabi@gmail.com>
2026-08-11 13:04:19 +0200

    Add files via upload

--- src/locales/locales.d.ts
+++ src/locales/locales.d.ts
@@ -23,4 +23,28 @@ declare interface Locales {
 	footer_rights: string;
 	footer_contacte: string;
 	footer_llicencia: string;
+
+	gc_meta_title: string;
+	gc_meta_description: string;
+	gc_manifest_p1: string;
+	gc_manifest_p2: string;
+	gc_manifest_p3: string;
+	gc_dues_vies_intro: string;
+	gc_oficial_title: string;
+	gc_oficial_text: string;
+	gc_diy_title: string;
+	gc_diy_text: string;
+	gc_descarrega_format: string;
+	gc_llicencia_title: string;
+	gc_llicencia_subtitle: string;
+	gc_us_personal_title: string;
+	gc_us_personal_text: string;
+	gc_us_comercial_title: string;
+	gc_us_comercial_text: string;
+	gc_logo_black_title: string;
+	gc_logo_black_desc: string;
+	gc_logo_primary_title: string;
+	gc_logo_primary_desc: string;
+	gc_logo_white_title: string;
+	gc_logo_white_desc: string;
 }
