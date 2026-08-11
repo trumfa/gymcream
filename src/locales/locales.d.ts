dc30e3f51968153d19481cf9d9567d5a7b30c214
trumfa <jordibabi@gmail.com>
2026-08-11 12:56:49 +0200

    Add files via upload

--- src/locales/locales.d.ts
+++ src/locales/locales.d.ts
@@ -2,6 +2,25 @@ declare interface Locales {
 	home_description: string;
 	about_description: string;
 	home: string;
-	about: string;
-	blog: string;
+
+	nav_inici: string;
+	nav_botiga: string;
+	nav_gymcreamer: string;
+	nav_descarregues: string;
+	nav_comunitat: string;
+
+	search_placeholder: string;
+	cart_title: string;
+	cart_empty: string;
+
+	btn_botiga: string;
+	btn_soc_gymcreamer: string;
+	btn_tornar_botiga: string;
+	btn_descarregar: string;
+	btn_enviar: string;
+	btn_veure_botiga: string;
+
+	footer_rights: string;
+	footer_contacte: string;
+	footer_llicencia: string;
 }
